0636e0bf4652dfb1f6c4cf9f98e4699008da89ac
Luke Wood <lewoodwebdev@gmail.com>
2017-05-05 23:16:23 +0000

    starting first pseudo-step

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ URL Shortener Microservice (Backend Certification Challenge for FreeCodeCamp)
 
 User Stories: 
 
--I can get the IP address, language and operating system for my browser.
--If I pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error instead.
+-I can pass a URL as a parameter and I will receive a shortened URL 
+in the JSON response.
+
+-If I pass an invalid URL that doesn't follow the valid http://www.example.com 
+format, the JSON response will contain an error instead.
+
 -When I visit that shortened URL, it will redirect me to my original link.
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 var express = require('express');
 var googl = require('goo.gl');
+var flash = require('express-flash');
 var app = express();
 var testURL = "http://google.com";
     
 // Set a developer key (_required by Google_; see http://goo.gl/4DvFk for more info.) 
-googl.setKey('avsufviKUVBILUBiuvsiuILUSVBIusbvuvLISUVBFi');
+googl.setKey('AIzaSyABWIT81tLxcPPvoyZO0N-oMWfXnH53ISY');
  
 // Get currently set developer key 
 googl.getKey();
@@ -16,20 +17,27 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/index.html');
 });
 
-app.get('/:string', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
+app.get('/new', function(req, res) {
+  res.send("new route success");
 
+
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Example app listening on port ' + process.env.PORT + '!');
+});
+
+
+
+  /*
+  res.setHeader('Content-Type', 'application/json');
+  var t = 'https://www.npmjs.com/package/valid-url';
 // Shorten a long url and output the result 
-googl.shorten(testURL)
+googl.shorten(t)
     .then(function (shortUrl) {
       res.send(JSON.stringify({shortUrl}));
     })
     .catch(function (err) {
         res.send(err.message);
     });
-
-});
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log('Example app listening on port ' + process.env.PORT + '!');
-})
+    */
